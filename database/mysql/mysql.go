cb95d194c34ee88df83db697a1f68f29cd731037
yuorei <yuorei71@gmail.com>
2023-03-12 16:19:01 +0900

    Sprintfを使用

--- database/mysql/mysql.go
+++ database/mysql/mysql.go
@@ -19,8 +19,8 @@ func NewMySQL() (*MySQL, error) {
 	DB_PORT := os.Getenv("DB_PORT")
 	DB_TZ := os.Getenv("DB_TZ")
 
-	dsn := DB_USER + ":" + DB_PASS + "@tcp(" + DB_PORT + ")/" + DB_NAME + "?charset=utf8mb4&parseTime=" + DB_TZ
-	fmt.Println(dsn)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=%s",DB_USER,DB_PASS,DB_PORT,DB_NAME,DB_TZ)
+
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	return &MySQL{
