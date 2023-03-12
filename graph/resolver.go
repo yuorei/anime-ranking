7bae7a6d4788a00908322b3946893e55981833c2
yuorei <yuorei71@gmail.com>
2023-03-12 16:35:45 +0900

    resolverにDBに追加

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,12 +1,15 @@
 package graph
 
-import "github.com/yuorei/anime-ranking/graph/model"
+import (
+	"github.com/yuorei/anime-ranking/database/mysql"
+	"github.com/yuorei/anime-ranking/graph/model"
+)
 
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{
-	db []*model.AnimeRanking
+type Resolver struct {
+	db    mysql.MySQL
 	users []*model.User
 }
