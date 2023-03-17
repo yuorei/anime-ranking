ebf8185a71910d3f833dadda9c6d9ed6dc112e93
yuorei <yuorei71@gmail.com>
2023-03-17 15:54:51 +0900

    定義の修正

--- database/mysql/mysql.go
+++ database/mysql/mysql.go
@@ -14,7 +14,11 @@ type MySQL struct {
 	Conn *gorm.DB
 }
 
-func NewMySQL() *MySQL {
+var db MySQL
+
+func NewMySQL() {
+	var err error
+
 	DB_USER := os.Getenv("DB_USER")
 	DB_PASS := os.Getenv("DB_PASS")
 	DB_NAME := os.Getenv("DB_NAME")
@@ -23,18 +27,23 @@ func NewMySQL() *MySQL {
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=%s", DB_USER, DB_PASS, DB_PORT, DB_NAME, DB_TZ)
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db.Conn, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("DB接続失敗")
 	}
 
-	return &MySQL{
-		Conn: db,
+}
+
+func CreateTable() {
+	if err := db.Conn.AutoMigrate(&table.User{}); err != nil {
+		log.Fatalf("Database create table failed失敗")
 	}
 }
 
-func (d *MySQL) CreateTable() {
-	if err := d.Conn.AutoMigrate(&table.User{}); err != nil {
-		log.Fatalf("Database create table failed失敗")
+func InsertUser(user table.User) (table.User, error) {
+	if err := db.Conn.Create(&user).Error; err != nil {
+		return user, err
 	}
+
+	return user, nil
 }
