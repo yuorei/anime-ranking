632c697e6442019f1fa3007676e547464fac174e
yuorei <yuorei71@gmail.com>
2023-03-23 18:15:54 +0900

    auth追加

--- graph/generated.go
+++ graph/generated.go
@@ -1165,8 +1165,28 @@ func (ec *executionContext) _Mutation_registerUserAnimeRanking(ctx context.Conte
 		}
 	}()
 	resTmp, err := ec.ResolverMiddleware(ctx, func(rctx context.Context) (interface{}, error) {
-		ctx = rctx // use context from middleware stack in children
-		return ec.resolvers.Mutation().RegisterUserAnimeRanking(rctx, fc.Args["input"].(model.NewAnimeRankingInput))
+		directive0 := func(rctx context.Context) (interface{}, error) {
+			ctx = rctx // use context from middleware stack in children
+			return ec.resolvers.Mutation().RegisterUserAnimeRanking(rctx, fc.Args["input"].(model.NewAnimeRankingInput))
+		}
+		directive1 := func(ctx context.Context) (interface{}, error) {
+			if ec.directives.Auth == nil {
+				return nil, errors.New("directive auth is not implemented")
+			}
+			return ec.directives.Auth(ctx, nil, directive0)
+		}
+
+		tmp, err := directive1(rctx)
+		if err != nil {
+			return nil, graphql.ErrorOnPath(ctx, err)
+		}
+		if tmp == nil {
+			return nil, nil
+		}
+		if data, ok := tmp.(*model.AnimeRankingPayload); ok {
+			return data, nil
+		}
+		return nil, fmt.Errorf(`unexpected type %T from directive, should be *github.com/yuorei/anime-ranking/graph/model.AnimeRankingPayload`, tmp)
 	})
 	if err != nil {
 		ec.Error(ctx, err)
