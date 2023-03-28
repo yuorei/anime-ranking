e39cd3a98a1322ceee7363cde94df99439384744
yuorei <yuorei71@gmail.com>
2023-03-28 15:21:50 +0900

    resolverの使っていないフィールド削除

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,15 +1,7 @@
 package graph
 
-import (
-	"github.com/yuorei/anime-ranking/database/mysql"
-	"github.com/yuorei/anime-ranking/graph/model"
-)
-
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct {
-	db    mysql.MySQL
-	users []*model.User
-}
+type Resolver struct{}
