7412d1eb5908e6afe8482bd81df577bdeaba9a61
yuorei <yuorei71@gmail.com>
2023-04-02 19:21:31 +0900

    SQLのログを出力するようにした

--- database/mysql/mysql.go
+++ database/mysql/mysql.go
@@ -9,6 +9,7 @@ import (
 	"github.com/yuorei/anime-ranking/database/table"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 type MySQL struct {
@@ -28,7 +29,17 @@ func NewMySQL() {
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=%s", DB_USER, DB_PASS, DB_PORT, DB_NAME, DB_TZ)
 
-	db.Conn, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			Colorful:                  false,       // Disable color
+		},
+	)
+
+	db.Conn, err = gorm.Open(mysql.Open(dsn), &gorm.Config{Logger: newLogger})
 	if err != nil {
 		log.Fatal("DB接続失敗")
 	}
