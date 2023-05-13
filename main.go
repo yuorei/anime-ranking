0e216d28709d99d5b46b82384ffa61dc8fbcbd70
yuorei <yuorei71@gmail.com>
2023-05-13 21:54:13 +0900

    CORSのURLを環境変数からにした

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 	"github.com/rs/cors"
 
 	"github.com/yuorei/anime-ranking/database/mysql"
-	"github.com/yuorei/anime-ranking/directives"
+	"github.com/yuorei/anime-ranking/directive"
 	"github.com/yuorei/anime-ranking/graph/generated"
 	"github.com/yuorei/anime-ranking/graph/resolver"
 	"github.com/yuorei/anime-ranking/middlewares"
@@ -22,8 +22,7 @@ import (
 const defaultPort = "8080"
 
 func main() {
-	var err error
-	err = godotenv.Load(".env")
+	err := godotenv.Load(".env")
 	if err != nil {
 		fmt.Printf("読み込み出来ませんでした: %v", err)
 	}
@@ -40,18 +39,17 @@ func main() {
 	router.Use(middlewares.AuthMiddleware)
 
 	c := generated.Config{Resolvers: &resolver.Resolver{}}
-	c.Directives.Auth = directives.Auth
+	c.Directives.Auth = directive.Auth
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(c))
 	// CORSの設定
 	corsOpts := cors.New(cors.Options{
-		AllowedOrigins: []string{"http://localhost:3000"}, // 許可するOriginを指定
+		AllowedOrigins: []string{os.Getenv("URL")}, // 許可するOriginを指定
 		AllowedMethods: []string{http.MethodGet, http.MethodPost, http.MethodOptions},
 		AllowedHeaders: []string{"Content-Type"},
 	})
 	router.PathPrefix("/query").Handler(corsOpts.Handler(srv))
 	router.PathPrefix("/").Handler(playground.Handler("GraphQL playground", "/query"))
-
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, router))
 }
